0a3a48eaa13f1c1ced766b834b6dc55bc0154bc2
Kumbukani1 <nyondokumbukani1@gmail.com>
2026-03-05 03:22:19 +0200

    Learnt how to separate code according to respective languages. This improved code readability and a service for good tracking in future

---
+++ Scripts/10-rock-paper-scissors.js
@@ -0,0 +1,69 @@
+ let score = JSON.parse(localStorage.getItem('score')) || {
+        wins: 0,
+        losses: 0,
+        ties: 0
+      };
+
+      function updateScore() {
+        document.querySelector('.js-score').innerHTML = 
+          `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
+      }
+      updateScore();
+
+      function playGame(playerMove) {
+        const computerMove = pickComputerMove();
+        let result = '';
+
+        if (playerMove === 'Scissors') {
+          if (computerMove === 'Rock') {
+            result = 'You lose!';
+            score.losses += 1;
+          } else if (computerMove === 'Paper') {
+            result = 'You win!';
+            score.wins += 1;
+          } else if (computerMove === 'Scissors') {
+            result = 'It is a tie!';
+            score.ties += 1;
+          }
+        } else if (playerMove === 'Paper') {
+          if (computerMove === 'Rock') {
+            result = 'You win!';
+            score.wins += 1;
+          } else if (computerMove === 'Paper') {
+            result = 'It is a tie!';
+            score.ties += 1;
+          } else if (computerMove === 'Scissors') {
+            result = 'You lose!';
+            score.losses += 1;
+          }
+        } else if (playerMove === 'Rock') {
+          if (computerMove === 'Rock') {
+            result = 'It is a tie!';
+            score.ties += 1;
+          } else if (computerMove === 'Paper') {
+            result = 'You lose!';
+            score.losses += 1;
+          } else if (computerMove === 'Scissors') {
+            result = 'You win!';
+            score.wins += 1;
+          }
+        }
+
+        localStorage.setItem('score', JSON.stringify(score));
+        updateScore();
+
+
+        document.querySelector('.js-results').innerHTML = `${result} `;
+        
+        document.querySelector('.js-moves').innerHTML = `      You played: <img src="Images/${playerMove}-emoji.png"
+       class="move-icon">. 
+         Computer played: <img src="Images/${computerMove}-emoji.png"
+        class="move-icon">`;
+        }
+
+      function pickComputerMove() {
+        let randomNumber = Math.random();
+        if (randomNumber < 1/3) return 'Rock';
+        if (randomNumber < 2/3) return 'Paper';
+        return 'Scissors';
+      }
